7aaacea71a653beaf7b08eeb8886cc4803bea170
Toni Díaz <tonidiazfdez@gmail.com>
2025-01-31 13:28:51 +0100

    Hacemos el codigo de error 404

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/pythonmongodb'
@@ -14,14 +15,36 @@ def create_user():
     email= request.json['email']
 
     if username and password and email:
-        mongo.db.users.insert_one({
+        hashed_password = generate_password_hash(password)
+        
+        id = mongo.db.users.insert_one(
+            {
             'username': username,
-            'password': password,
+            'password': hashed_password,
             'email': email
-        })
+            }
+        )
 
+        response = {
+            'id': str(id),
+            'username': username,
+            'password': hashed_password,
+            'email': email
+        }
+
+        return response
+    else:
+        return not_found(), 404
     
-    return {"message": "recieved"}
+@app.errorhandler(404)
+def not_found(error=None):
+
+    message = {
+        'message': 'resource not found ' + request.url,
+        'status': 404
+    }
+
+    return message
 
 if __name__ == '__main__':
 
